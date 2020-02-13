308be30c4fa290edb2121818ac5f3acdb93046b6
Jordi <jordi.barcia@enti.cat>
2020-02-13 12:18:30 +0100

    Added function "Repeticion", includes a "for"

--- HelloPartner/Source.cpp
+++ HelloPartner/Source.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+void Repeticion() 
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "Hello World" << std::endl; // Imprimeix "Hello World 10 vegades"
+	}
+}
 int main() 
 {
 	std::cout << "Hello Adrian" << std::endl;
 	std::cout << "Hello Jordi" << std::endl;
+	Repeticion();
 	return 0;
 }
