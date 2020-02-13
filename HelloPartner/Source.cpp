4af245903e77ed9e74135b2efebe84e354c392ce
Adrian <Puig>
2020-02-13 12:19:51 +0100

    contador 1 --> 100

--- HelloPartner/Source.cpp
+++ HelloPartner/Source.cpp
@@ -4,5 +4,15 @@ int main()
 {
 	std::cout << "Hello Adrian" << std::endl;
 	std::cout << "Hello Jordi" << std::endl;
+
+
+
+	for (int i = 0; i < 100; i++) {
+
+		sts::cout << i << std::endl;
+	}
+
+
+
 	return 0;
 }
